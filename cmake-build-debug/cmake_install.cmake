384948d3c662d43a924a4238d4675574bec53078
ahmedtomy <162179295+ahmedtomy@users.noreply.github.com>
2024-08-19 05:17:24 +0200

    yaraaaaab

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Admin/Desktop/bankkk
+# Install script for directory: C:/Users/Admin/Desktop/bank system/bankkk
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Admin/Desktop/bankkk/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Admin/Desktop/bank system/bankkk/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cctype>
 using namespace std;
+
 bool checkname(const string& str) {
     for (int i = 0; i < (int)str.length(); i++) {
         if (!isalpha(str[i])) {
@@ -10,89 +11,209 @@ bool checkname(const string& str) {
     }
     return true;
 }
-class Validation{
+
+class Validation {
 public:
-    static bool IsValidpassword(string pass){
-            return (pass.length()>=8 && pass.length()<=20);
-        }
-    static bool IsValidName(string name){
-        return (name.length()>=8 && name.length()<=20 && checkname(name));
+    static bool IsValidpassword(string pass) {
+        return (pass.length() >= 8 && pass.length() <= 20);
     }
-    };
+    static bool IsValidName(string name) {
+        return (name.length() >= 8 && name.length() <= 20 && checkname(name));
+    }
+    static bool IsValidSalary(double salary) {
+        return (salary >= 5000);
+    }
+};
 
-
-
-class Person{
+class Person {
 protected:
-     int id;string name;string password;
+    int id;
+    string name;
+    string password;
+    static int nextid;
+
 public:
     Person() {
-        id=10000;
-        id++;
+        id = nextid++;
+    }
 
+    Person(string name, string password) {
+        id = nextid++;
+        this->name = name;
+        this->password = password;
     }
-    Person(string name,string password) {
-        this->name=name;
-        this->password=password;
+
+    Person(int id, string name, string password) {
+        this->id = id;
+        this->name = name;
+        this->password = password;
     }
-    void setname(string n){
+
+    void setname(string n) {
         if (Validation::IsValidName(n))
-        name=n;
+            name = n;
         else
-            cerr<<"name not valid";
+            cerr << "name not valid" << endl;
     }
+
     void setpassword(string p) {
         if (Validation::IsValidpassword(p))
             password = p;
         else
-            cerr<<"password not valid";
+            cerr << "password not valid" << endl;
     }
-    string getpassword(){
+
+    string getpassword() {
         return password;
     }
-    string getname(){
+
+    string getname() {
         return name;
     }
-    int getid(){
+
+    int getid() {
         return id;
     }
+
+
+    virtual void display() = 0;
 };
-class Employee :public Person  {
-private :
-    //data 
-    double balance;
+
+int Person::nextid = 10000;
+
+class Employee : public Person {
+private:
+    double salary;
+
 public:
-    //con
-    Employee() {
-        balance = 0;
+    Employee() : Person() {
+        salary = 0;
     }
-    Employee(int i, string n, string p, double b) {
+
+    Employee(int i, string n, string p, double s) {
         id = i;
         name = n;
         password = p;
-        balance = b;
+        salary = s;
     }
-    //sett
+
+    void setsalary(double s) {
+        if (Validation::IsValidSalary(s))
+            salary = s;
+        else
+            cout << "salary not accepted less than 5000" << endl;
+    }
+
+    double getsalary() {
+        return salary;
+    }
+
+    void display() {
+        cout << "Employee details: " << endl;
+        cout << "ID = " << id << endl;
+        cout << "Name = " << name << endl;
+        cout << "Password = " << password << endl;
+        cout << "Salary = " << salary << endl;
+    }
+};
+
+class Admin : public Person {
+private:
+    double salary;
+
+public:
+    Admin() : Person() {
+        salary = 0.0;
+    }
+    Admin(double s){
+        this->salary=s;
+    }
+    Admin(int id, string name, string password, double salary) : Person(id, name, password) {
+        this->salary = salary;
+    }
+
+    void setsalary(double s) {
+        if (Validation::IsValidSalary(s))
+            salary = s;
+        else
+            cout << "not valid salary less than 5000";
+    }
+
+    double getsalary() {
+        return salary;
+    }
+
+    void display() {
+        cout << "Admin details: " << endl;
+        cout << "ID = " << id << endl;
+        cout << "Name = " << name << endl;
+        cout << "Password = " << password << endl;
+        cout << "Salary = " << salary << endl;
+    }
+};
+
+class Client : public Person {
+private:
+    double balance;
+
+public:
+    Client() : Person() {
+        balance = 0;
+    }
+
+    Client(double balance, int id, string name, string password) : Person(id, name, password) {
+        this->balance = balance;
+    }
+
     void setbalance(double b) {
-        balance = b;
+        this->balance = b;
     }
-    //get 
-    double gitbalance() {
+
+    double getbalance() {
         return balance;
     }
-    //methods 
-    void disblay() {
-        cout << "id =" << id << endl;
-        cout << "name =" << name << endl; 
-        cout << "password =" << name << endl;
-        cout << "balance = " << balance << endl;
+
+    double deposit(double amount) {
+        balance += amount;
+        return balance;
     }
 
+    double withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            cout << "Not enough balance" << endl;
+        }
+        return balance;
+    }
 
+    void transferto(Client& m, double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            m.deposit(amount);
+        } else {
+            cout << "Not enough balance" << endl;
+        }
+    }
+
+    double checkbalance() {
+        return balance;
+    }
+
+    void display() {
+        cout << "Client details: " << endl;
+        cout << "ID = " << id << endl;
+        cout << "Name = " << name << endl;
+        cout << "Password = " << password << endl;
+        cout << "Balance = " << balance << endl;
+    }
 };
-int main(){
-cout<<"welcome to bank system";
-cout << "salah essa ";
+
+int main() {
+    cout << "Welcome to the bank system" << endl;
+
+    Admin admin(30000);
+    admin.display();
+    Employee employee;
+    cout<<employee.getid();
 }
-
-
